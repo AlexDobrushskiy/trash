224b05aa4d06df456aa2293cce11380a39d99f38
Alex Dobrushskiy <adobrushskiy@asdco.ru>
2013-12-19 12:08:21 +0400

    Generator for test data

---
+++ home_task/datagenerator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This module generates test data for PyExtSort and puts it to gen_data.dat file
+"""
+__author__ = 'Alex Dobrushskiy'
+
+from string import letters, digits, lowercase
+from random import choice, randint
+from time import time
+from datetime import datetime
+
+data = []
+now = int(time())
+for i in range(20):
+    uname_len, domain_len = randint(3, 20), randint(3, 20)
+    rand_word = lambda x: ''.join(choice(letters+digits) for i in range(x))
+    email = rand_word(uname_len) + '@' + rand_word(domain_len) + '.' + \
+            ''.join(choice(lowercase) for i in range(3))
+    rand_time = datetime.fromtimestamp(randint(1, now)).isoformat()
+    data.append(email + ' ' + rand_time + ' ' + str(i) + '\n')
+
+f = open('gen_data.dat', 'w')
+for st in data:
+    f.write(st)
---
+++ home_task/pyextsort.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Alex Dobrushskiy'
+
+# from argparse import ArgumentParser
+#
+# parser = ArgumentParser(description='PyExtSort arguments parser')
+
+f = open('input.dat')
+
